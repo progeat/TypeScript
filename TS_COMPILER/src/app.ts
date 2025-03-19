8c07bbd80b3dc20dc4b59d32d66aa724058106a6
Khokhlov Andrey <progeat@mail.ru>
2025-03-19 13:38:52 +0300

    Компилятор (завершение знакомства)

--- TS_COMPILER/src/app.ts
+++ TS_COMPILER/src/app.ts
@@ -1,1 +1,11 @@
 // Описание урока в конфиг файле
+
+function log(a: number) {}
+
+// const node = document.querySelector('.test')! as HTMLInputElement;
+
+// node.value = '30';
+
+const found = [{ a: 1 }, { b: 2 }].find((n) => n.a === 3);
+
+found!.a = 3;
