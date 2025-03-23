3df691694d60cc81c7e4cc0850b9b39394ef686e
Andrey Khokhlov <progeat@mail.ru>
2025-03-23 15:39:31 +0300

    Классы в TS(завершение)

--- TS_CLASSES/app.js
+++ TS_CLASSES/app.js
@@ -37,7 +37,7 @@ class User extends Human {
     }
     isProgrammer() {
         // нужно в ts конфиге включить поле "noImplicitOverride"
-        console.log(super.isProgrammer()); // для вызова родительского метода
+        console.log(super.isProgrammer() /* для вызова родительского метода */);
         return true;
     }
 }
@@ -50,3 +50,81 @@ user.name = 'Evgen';
 console.log(user.hobbies);
 console.log(user._name);
 console.log(user.isProgrammer());
+/* Модификаторы доступа */
+class Figure {
+    constructor() {
+        this.size = 10;
+        this.color = 'red';
+        this.id = Math.random();
+    }
+    getId() {
+        return this.id;
+    }
+}
+Figure.BASE_TYPE = 'FIGURE';
+class Box extends Figure {
+    static logId() {
+        console.log(Math.random());
+    }
+    getInfo() {
+        return {
+            size: this.size,
+            color: this.color,
+            id: this.getId(),
+        };
+    }
+}
+Box.TYPE = 'BOX';
+const box = new Box();
+Box.logId();
+console.log(box.getInfo());
+console.log(Box.TYPE);
+console.log(Box.BASE_TYPE);
+/* Сокращенный метод создания класса */
+class Car {
+    constructor(model, color) {
+        this.model = model;
+        this.color = color;
+    }
+}
+const ford = new Car('Ford', 'Blue');
+class Car2 {
+    constructor(model, color) {
+        this.model = model;
+        this.color = color;
+    }
+}
+const honda = new Car2('Honda', 'White');
+class Component {
+    constructor() {
+        this.hasChanged = false;
+        // onDestroy?(abort: boolean): void {
+        //   if (abort) {
+        //     console.log('Component on destroy')
+        //   }
+        // }
+    }
+    onInit() {
+        console.log('Component on init');
+    }
+    onChange(data) {
+        if (data > 1) {
+            return true;
+        }
+        return false;
+    }
+}
+/* Абстрактные классы */
+class Logger {
+    table(data) {
+        console.table(data);
+    }
+}
+class MessageLogger extends Logger {
+    log(message) {
+        console.log(message);
+    }
+}
+const logger = new MessageLogger();
+logger.log('Hello');
+logger.table({ a: 'Привет', b: 'Мир' });
--- TS_CLASSES/app.ts
+++ TS_CLASSES/app.ts
@@ -67,4 +67,119 @@ console.log(user.hobbies);
 console.log(user._name);
 console.log(user.isProgrammer());
 
-// Пересмотреть урок Модификаторы доступа
+/* Модификаторы доступа */
+
+class Figure {
+  static readonly BASE_TYPE = 'FIGURE';
+
+  public size: number = 10;
+  public color: string = 'red';
+  public id: number;
+
+  constructor() {
+    this.id = Math.random();
+  }
+
+  protected getId(): number {
+    return this.id;
+  }
+}
+
+class Box extends Figure {
+  static readonly TYPE = 'BOX';
+
+  static logId() {
+    console.log(Math.random());
+  }
+
+  getInfo() {
+    return {
+      size: this.size,
+      color: this.color,
+      id: this.getId(),
+    };
+  }
+}
+
+const box = new Box();
+
+Box.logId();
+
+console.log(box.getInfo());
+console.log(Box.TYPE);
+console.log(Box.BASE_TYPE);
+
+/* Сокращенный метод создания класса */
+
+class Car {
+  model: string;
+  color: string;
+
+  constructor(model: string, color: string) {
+    this.model = model;
+    this.color = color;
+  }
+}
+
+const ford = new Car('Ford', 'Blue');
+
+class Car2 {
+  constructor(public model: string, private color: string) {}
+}
+
+const honda = new Car2('Honda', 'White');
+
+/* Реализация интерфесов классами */
+
+interface Lifecycle {
+  onInit(): void;
+
+  onDestroy?(abort: boolean): void;
+}
+
+interface ComponentOnChange {
+  hasChanged: boolean;
+
+  onChange(data: number): boolean;
+}
+
+class Component implements Lifecycle, ComponentOnChange {
+  hasChanged: boolean = false;
+
+  onInit(): void {
+    console.log('Component on init');
+  }
+
+  onChange(data: number): boolean {
+    if (data > 1) {
+      return true;
+    }
+    return false;
+  }
+
+  // onDestroy?(abort: boolean): void {
+  //   if (abort) {
+  //     console.log('Component on destroy')
+  //   }
+  // }
+}
+
+/* Абстрактные классы */
+
+abstract class Logger {
+  abstract log(message: string): void;
+
+  table(data: object) {
+    console.table(data);
+  }
+}
+
+class MessageLogger extends Logger {
+  log(message: string): void {
+    console.log(message);
+  }
+}
+const logger = new MessageLogger();
+
+logger.log('Hello');
+logger.table({ a: 'Привет', b: 'Мир' });
